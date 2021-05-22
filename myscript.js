f5ad4acd008702dbfa3c8a0c42aac526e742e432
Sneha Banga <79856550+starcoder01@users.noreply.github.com>
2021-05-22 16:08:18 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,11 +1,8 @@
 function myfun()
 {
     var a=document.getElementById("myDropdown");
-    if(a.style.display=="none"){
-        a.style.display="flex";
-        a.style.flexDirection="column";
-        a.style.position="relative";
-        a.style.top="10%";
+    if(a.style.display ==="none"){
+        a.style.display="block";
     }
     else
         a.style.display="none";
